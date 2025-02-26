7ce37f9d90179ff3a4374ed61d8867e032eebecf
yut0takagi <yutotkg.1040@gmail.com>
2025-02-26 09:36:21 +0900

    01 Firebase設定を取り消しました。

--- new_flask_app/dx_app/models/classroom.py
+++ new_flask_app/dx_app/models/classroom.py
@@ -11,3 +11,11 @@ class Period(db.Model):
     organization = db.Column(db.String(100), db.ForeignKey('Organization.name'), nullable=False)
     name=db.Column(db.String(100), nullable=False)
     
+class Attention(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    organization = db.Column(db.String(100), db.ForeignKey('Organization.name'), nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    attention_title=db.Column(db.String(100), nullable=False)
+    statement = db.Column(db.String(2000), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    role = db.Column(db.String(50), nullable=False)
